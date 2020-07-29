c41b5e8e7346c02932dee85d8daf75d909208607
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-07-30 00:19:47 +0530

    Arithmetic Operators

--- Python/HackerRank/Arithmetic Operators.py
+++ Python/HackerRank/Arithmetic Operators.py
@@ -1,4 +1,5 @@
-/* Task
+'''
+Task
 The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
 
 1. The first line contains the sum of the two numbers.
@@ -40,9 +41,10 @@ Explanation 0
 
 3+2=5
 3-2+1
-3*2=6  */
+3*2=6
+'''
 
-// Code:
+# Code:
 
 if __name__ == '__main__':
     a = int(input())
